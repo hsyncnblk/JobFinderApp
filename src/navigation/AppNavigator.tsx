8e048904b5b39a1d66b8501bf59c3350c7028bab
huseyin cinibulak <huseyincnblk@gmail.com>
2024-09-07 10:47:24 +0300

    createAccountScreen ui completed

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from '../screens/SplashScreen';
 import LoginScreen from '../screens/LoginScreen';
+import CreateAccountScreen from '../screens/CreateAccountScreen';
 
 
 const Stack = createStackNavigator();
@@ -11,10 +13,11 @@ const AppNavigator = () => {
 
   return (
     <NavigationContainer>
-           <Stack.Navigator initialRouteName="Splash">
+        <Stack.Navigator initialRouteName="Splash">
         <Stack.Screen name="Splash" component={SplashScreen} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
-        
+        <Stack.Screen name="CreateAccount" component={CreateAccountScreen} options={{ headerTitle: "" }} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/screens/CreateAccountScreen.tsx
@@ -0,0 +1,115 @@
+
+
+import React, { useContext } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { useNavigation } from '@react-navigation/native';
+
+
+const CreateAccountScreen = () => {
+  const { control, handleSubmit ,reset } = useForm();
+  const navigation = useNavigation();
+
+  
+  const onSubmit = (data: { email: string; password: string }) => {
+    console.log("user created")
+  };
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Create Account</Text>
+
+      <Text style={styles.label}>Email</Text>
+      <Controller
+        control={control}
+        name="email"
+        rules={{ required: 'Email is required' }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Email"
+            keyboardType="email-address"
+          />
+        )}
+      />
+
+      <Text style={styles.label}>Password</Text>
+      <Controller
+        control={control}
+        name="password"
+        rules={{ required: 'Password is required' }}
+        render={({ field: { onChange, value } }) => (
+          <TextInput
+            style={styles.input}
+            onChangeText={onChange}
+            value={value}
+            placeholder="Password"
+            secureTextEntry
+          />
+        )}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleSubmit(onSubmit)}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+     
+   
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 30,
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+ 
+  label: {
+    color: "black",
+    fontSize: 16,
+    fontWeight: '600',
+    marginBottom: 10,
+  },
+  input: {
+    color: 'black',
+    height: 50,
+    borderColor: '#000',
+    borderWidth: 4,
+   
+    paddingHorizontal: 10,
+    marginBottom: 25,
+  },
+  button: {
+  
+    paddingVertical: 10,
+    
+    borderColor: 'black',
+    borderWidth: 4,
+    marginBottom: 40,
+    borderBottomWidth: 7,
+    borderRightWidth: 7,
+  },
+  buttonText: {
+    color: 'black',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  error: {
+    color: 'red',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+});
+
+export default CreateAccountScreen;
